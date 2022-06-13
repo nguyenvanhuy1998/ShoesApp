04c49198b2c42ca87fc169276ea1266171cadd07
Huy Nguyễn <huygemini1998@gmail.com>
2022-06-13 11:33:13 +0700

    +setup background image in login screen

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,20 +1,41 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 import React from 'react';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {images, theme} from '../../constants';
+import LinearGradient from 'react-native-linear-gradient';
 
 const Login = () => {
-  const {top} = useSafeAreaInsets();
   return (
-    <View
-      style={{
-        flex: 1,
-        paddingTop: top,
-      }}>
-      <Text>Login</Text>
+    <View style={styles.flexOne}>
+      <View style={styles.flexTwo}>
+        <Image
+          resizeMode="cover"
+          source={images.bgLogin}
+          style={styles.background_image}
+        />
+      </View>
+      <View style={styles.flexOne} />
+      <LinearGradient
+        style={styles.gradient_container}
+        colors={['transparent', theme.COLORS.white, theme.COLORS.white]}>
+        {/* form login */}
+      </LinearGradient>
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flexOne: {
+    flex: 1,
+  },
+  flexTwo: {
+    flex: 2,
+  },
+  background_image: {width: '100%', height: '100%'},
+  gradient_container: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+});
