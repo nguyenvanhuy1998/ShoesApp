7ebfca1d00cb9c4c9ded1256a66123d58dfdebcf
thaovy <thaovyluuthanh@gmail.com>
2022-06-25 15:12:07 +0700

    screen detail

--- src/screens/Detail/components/InformationProduct/components/InformationProductBottom/index.js
+++ src/screens/Detail/components/InformationProduct/components/InformationProductBottom/index.js
@@ -1,7 +1,6 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
 import styles from '../InformationProductBottom/styles';
-import {icons} from '../../../../../../constants';
 
 const InformationProductBottom = () => {
   return (
